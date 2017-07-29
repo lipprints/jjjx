d39c25383fc86cf970b9f560d77edbb60f666560
AMing <13120241790@163.com>
2017-07-29 08:33:18 +0800

    add search

---
+++ JJJX/app/src/main/java/com/jjjx/activity/SearchActivity.java
@@ -0,0 +1,20 @@
+package com.jjjx.activity;
+
+import android.os.Bundle;
+
+import com.jjjx.R;
+
+
+/**
+ * Created by AMing on 17/7/27.
+ * Company RongCloud
+ */
+
+public class SearchActivity extends BaseActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_search);
+        setTitle("搜索");
+    }
+}
