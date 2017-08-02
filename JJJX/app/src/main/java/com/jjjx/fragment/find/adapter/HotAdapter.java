e560947239000ef0f1b8dd4c4abc165a218789dd
xuzuo <624117230@qq.com>
2017-08-02 12:27:28 +0800

    测测试提交

---
+++ JJJX/app/src/main/java/com/jjjx/fragment/find/adapter/HotAdapter.java
@@ -0,0 +1,20 @@
+package com.jjjx.fragment.find.adapter;
+
+import com.jjjx.app.adapter.RvPureDataAdapter;
+import com.jjjx.app.adapter.util.RvViewHolder;
+
+/**
+ * Created by xz on 2017/8/2 0002.
+ */
+
+public class HotAdapter extends RvPureDataAdapter{
+    @Override
+    public int getItemLayout(int viewType) {
+        return 0;
+    }
+
+    @Override
+    public void onBindViewHolder(RvViewHolder holder, int position) {
+
+    }
+}
