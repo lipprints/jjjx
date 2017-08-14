38f5f1efbaebdf01c9977d74dec0c37e7b91fa5b
AMing <13120241790@163.com>
2017-08-14 11:26:15 +0800

    发现发布图片页

--- JJJX/app/src/main/java/com/jjjx/activity/FindImageActivity.java
+++ JJJX/app/src/main/java/com/jjjx/activity/FindImageActivity.java
@@ -2,6 +2,7 @@ package com.jjjx.activity;
 
 import android.os.Bundle;
 
+import com.facebook.drawee.view.SimpleDraweeView;
 import com.jjjx.R;
 import com.jjjx.data.response.FindDataResponse;
 
@@ -10,11 +11,16 @@ import com.jjjx.data.response.FindDataResponse;
  * Company RongCloud
  */
 public class FindImageActivity extends BaseActivity {
+
+    SimpleDraweeView simpleDraweeView;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_find_image);
         setTitle("发现详情");
         FindDataResponse.ParaEntity.DiscoverInfoEntity entity = getIntent().getParcelableExtra("FindImageEntity");
+        simpleDraweeView = (SimpleDraweeView) findViewById(R.id.find_image);
+        simpleDraweeView.setImageURI(entity.getPicture());
     }
 }
